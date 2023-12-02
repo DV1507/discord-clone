40db80ef7e4acdf15f9c7365a6fdd372c8c78094
DV1507 <78662713+DV1507@users.noreply.github.com>
2023-12-02 17:51:50 +0530

    feat: integrated sign in modal and upload thing service

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
 const SetupPage = async () => {
   const profile = await initialProfile();
   const server = await db.server.findFirst({
@@ -16,7 +17,7 @@ const SetupPage = async () => {
   if (server) {
     return redirect(`/servers/${server.id}`);
   }
-  return <div>create a server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
---
+++ components/file-upload.tsx
@@ -0,0 +1,4 @@
+"use-client";
+export const FileUpload = () => {
+  return <div>File Upload Component</div>;
+};
