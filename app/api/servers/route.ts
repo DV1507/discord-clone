cebff7beac259481f62a9a7ff57c8121602698ca
DV1507 <78662713+DV1507@users.noreply.github.com>
2023-12-14 11:31:55 +0530

    feat: added create server functionality

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -1,16 +1,19 @@
 import { v4 as uuidv4 } from "uuid";
+import { NextResponse } from "next/server";
 import { MemberRole } from "@prisma/client";
+
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     const { name, imageUrl } = await req.json();
     const profile = await currentProfile();
+
     if (!profile) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
+
     const server = await db.server.create({
       data: {
         profileId: profile.id,
@@ -25,9 +28,10 @@ export async function POST(req: Request) {
         },
       },
     });
-    NextResponse.json(server);
+
+    return NextResponse.json(server);
   } catch (error) {
-    console.log("server_post", error);
-    return new NextResponse("Internal", { status: 401 });
+    console.log("[SERVERS_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
 }
